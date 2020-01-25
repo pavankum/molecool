5a631d105be2b58d2983d11318887892acfd538b
pavankum <pavankum@buffalo.edu>
2020-01-25 15:12:18 -0500

    adding help documentation to open_pdb function in io/pdb.py file

--- molecool/io/pdb.py
+++ molecool/io/pdb.py
@@ -7,7 +7,28 @@ module for manipulating pdb format files
 import numpy as np
 
 def open_pdb(file_location):
-    # This function reads in a pdb file and returns the atom names and coordinates.
+    """
+	This function reads in a atomic coordinates file in protein database (pdb) format and returns the atom names and coordinates.
+	
+	Parameters
+	----------
+	file_location: string
+		directory of the file location along with the filename to read
+		
+	Returns
+	-------
+	symbols: string
+		atomic symbols
+	coordinates: np.ndarray
+		atomic [x, y, z] coordinates
+		
+	Examples
+	--------
+	>>> open_pdb(oxygen.pdb)
+	O 0.0 0.0 0.0
+	
+	"""
+	
     with open(file_location) as f:
         data = f.readlines()
         
@@ -23,4 +44,4 @@ def open_pdb(file_location):
     # Convert list to numpy array
     coordinates = np.array(coordinates)
     
-    return sym, coordinates
+    return symbols, coordinates
