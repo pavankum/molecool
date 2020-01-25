4ac31ef3fd51900bca3e54fa4c098f5a39d5bcb0
pavankum <pavankum@buffalo.edu>
2020-01-25 11:49:26 -0500

    Update functions.py
    Changing a file online

--- molecool/functions.py
+++ molecool/functions.py
@@ -23,9 +23,9 @@ def canvas(with_attribution=True):
         Compiled string including quote and optional attribution
     """
 
-    quote = "The code is but a canvas to our imagination!!!"
+    quote = "Change the quote"
     if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
+        quote += "\n\t- The Universe"
     return quote
 
 
